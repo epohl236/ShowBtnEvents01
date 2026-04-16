97e9f78dc0daf2893667fa51dbe8301bbf5d80e5
elmar <elmar_pohl@hotmail.com>
2026-04-17 00:33:55 +0200

    Options to have event handlers throw an exception

--- ShowBtnEvents01.js
+++ ShowBtnEvents01.js
@@ -1,11 +1,25 @@
 // Showing button events
 
+/** Maximum number of messages in the output textarea */
 const MAXNUMMSGS = 20;
 
+/** Expected maximum length of a series of events in msec */
+const MSGBLOCKMSECS = 250;
+
+/** true: after start of a series of events, waitig for end */
 var inMsgBlock = false;
+
+/** Messages to be shown in output textarea */
 var aMsgs = [];
 
 
+/**
+ * Timeout handler
+ * 
+ * Used to insert an empty line separator after the last message
+ * in the output textarea when no events were triggered after a
+ * certain time (see MSGBLOCKMSECS)
+ */
 function handleTimeout()
 {
     printMsg ("");
@@ -13,6 +27,11 @@ function handleTimeout()
 }
 
 
+/**
+ * Show a message in the output textarea
+ * 
+ * @param {string} strMsg - Message to be shown
+ */
 function printMsg (strMsg)
 {
     let textArea01 = document.getElementById ("textArea01");
@@ -35,16 +54,31 @@ function printMsg (strMsg)
     if (!inMsgBlock)
     {
         inMsgBlock = true;
-        setTimeout (handleTimeout, 250);
+        setTimeout (handleTimeout, MSGBLOCKMSECS);
     }
 }
 
+
+/**
+ * Check whether to throw an exception for events of button01.
+ * The result depends on the selection of radio button group rb_button01.
+ * 
+ * @param {string} strEventType - One of "click", "pointerdown", or "touchstart"
+ * @returns true if the handler should throw an exception for this event
+ */
+function button01ThrowAt (strEventType)
+{
+    let rb_button01_chkd = document.querySelector ("input[name='rb_button01']:checked");
+    return rb_button01_chkd != null && rb_button01_chkd.value == strEventType;
+}
 
 
 function handleButton01Click()
 {
     console.log ("click");
     printMsg ("btn: click");
+    if (button01ThrowAt ("click"))
+        throw new Error ("Button 01 click exception");
 }
 
 
@@ -52,6 +86,8 @@ function handleButton01Ptr()
 {
     console.log ("pointerdown");
     printMsg ("btn: pointerdown");
+    if (button01ThrowAt ("pointerdown"))
+        throw new Error ("Button 01 pointerdown exception");
 }
 
 
@@ -59,6 +95,22 @@ function handleButton01TouchStart()
 {
     console.log ("touchstart");
     printMsg ("btn: touchstart");
+    if (button01ThrowAt ("touchstart"))
+        throw new Error ("Button 01 touchstart exception");
+}
+
+
+/**
+ * Check whether to throw an exception for events of inputBtn01.
+ * The result depends on the selection of radio button group rb_inpBtn01.
+ * 
+ * @param {string} strEventType -  One of "click", "pointerdown", or "touchstart"
+ * @returns true if the handler should throw an exception for this event
+ */
+function inputBtn01ThrowAt (strEventType)
+{
+    let rb_inputBtn01_chkd = document.querySelector ("input[name='rb_inpBtn01']:checked");
+    return rb_inputBtn01_chkd != null && rb_inputBtn01_chkd.value == strEventType;
 }
 
 
@@ -66,6 +118,8 @@ function handleInputBtn01Click()
 {
     console.log ("click");
     printMsg ("inp: click");
+    if (inputBtn01ThrowAt ("click"))
+        throw new Error ("Input 01 click exception");
 }
 
 
@@ -73,6 +127,8 @@ function handleInputBtn01Ptr()
 {
     console.log ("pointerdown");
     printMsg ("inp: pointerdown");
+    if (inputBtn01ThrowAt ("pointerdown"))
+        throw new Error ("Input 01 pointerdown exception");
 }
 
 
@@ -80,7 +136,10 @@ function handleInputBtn01TouchStart()
 {
     console.log ("touchstart");
     printMsg ("inp: touchstart");
+    if (inputBtn01ThrowAt ("touchstart"))
+        throw new Error ("Input 01 touchstart exception");
 }
+
 
 function installButtonHandlers()
 {
